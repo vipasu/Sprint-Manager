bc45180f30a4d471b1491bb6788171c99133ce28
Vincent Su <phazedphantom@gmail.com>
2013-11-08 22:14:07 -0500

    Initial settings and task structure

--- goals.js
+++ goals.js
@@ -2,6 +2,8 @@
 // it is backed by a MongoDB collection named "goals".
 
 Goals = new Meteor.Collection("goals");
+Tasks = new Meteor.Collection("tasks");
+Settings = new Meteor.Collection("settings");
 
 if (Meteor.isClient) {
   Template.leaderboard.goals = function () {
@@ -28,20 +30,38 @@ if (Meteor.isClient) {
       Session.set("selected_goal", this._id);
     }
   });
+
+  Template.goal.tasks = function(){
+      return Tasks.find({goal: this.name});
+
+  }
+
+  Template.addTask.events = {
+      'submit': function(err, task) {
+          err.preventDefault();
+          var newTask =  {
+              name: task.find("#name").value,
+              goal: task.find("#category").value,
+              hours: task.find("#hours").value,
+          };
+          Tasks.insert(newTask);
+      }
+  }
 }
 
 // On server startup, create some goals if the database is empty.
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    if (Goals.find().count() === 0) {
-      var names = ["Ada Lovelace",
-                   "Grace Hopper",
-                   "Marie Curie",
-                   "Carl Friedrich Gauss",
-                   "Nikola Tesla",
-                   "Claude Shannon"];
-      for (var i = 0; i < names.length; i++)
-        Goals.insert({name: names[i], score: Math.floor(Random.fraction()*10)*5});
+    if (Goals.find().count() == 0) {
+      var names = ["Health",
+                   "School",
+                   "Social"];
+     Goals.insert({name: "Health", tasks: ["Exercise", "Sleep"]});
+    }
+    var now = new Date();
+    var username = 'test';
+    if (Settings.find({name: username}).count() === 0) {
+        Settings.insert({name: username, start: now, sprint: now, hours_per_day: 8, days_per_week: 5});
     }
   });
 }
