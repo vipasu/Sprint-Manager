e86e48614acb82edf11261223d3d10cb778a781b
Charles <charles@charles-VirtualBox.(none)>
2013-11-08 23:21:05 -0800

    Adding functions to be added later for buttons in script
     Committer: Charles <charles@charles-VirtualBox.(none)>
    
     On branch charles
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   util.js

--- util.js
+++ util.js
@@ -22,9 +22,32 @@ var calc_sprint_days_left = function(last_sprint, weeks_per_sprint) {
 var update_settings = function(id, field, value) {
   Settings.update(
 		  { _id : id },
-		  { $set: {field: value} }
+		  { $set: {field: value} },
+		  { multi: false},
 		  );
 };
+
+var remove_task = function(id) {
+  Tasks.remove({ _id : id });
+};
+
+// Can reverse functionality by doing negative hours
+var add_hours = function(id, hours) {
+  Tasks.update(
+		  { _id : id},
+		  { $inc: {hours_done: hours} },
+		  { multi: false},
+		  );
+  var task = Tasks.findOne({_id : id});
+  var hours_left = task.hours - task.hours_done;
+  var task_done = hours_left <= 0;
+};
+
+var finish_task = function(id) {
+  var task = Tasks.findOne({_id : id});
+  add_hours(id, task.hours - task.hours_done);
+};
+
 
 // TODO: Messages.find().fetch() -> array of objects
 // Iterate over message
