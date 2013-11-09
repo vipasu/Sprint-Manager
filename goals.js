26d009aa209640f2cf2bce0014ec3b306bacd4e2
Vincent Su <phazedphantom@gmail.com>
2013-11-09 11:34:07 -0500

    Added Sprint handling

--- goals.js
+++ goals.js
@@ -1,13 +1,52 @@
 // Set up a collection to contain goal information. On the server,
 // it is backed by a MongoDB collection named "goals".
 
+Sprints = new Meteor.Collection("sprints");
 Goals = new Meteor.Collection("goals");
 Tasks = new Meteor.Collection("tasks");
 Settings = new Meteor.Collection("settings");
 
 if (Meteor.isClient) {
-    Template.leaderboard.goals = function () {
-        return Goals.find({}, {sort: {score: -1, name: 1}});
+
+    var clearUser = function() {
+        while (Settings.findOne() !== 'undefined'){
+            Settings.remove({_id : Settings.findOne()._id});
+        }
+
+    };
+
+    Handlebars.registerHelper('newUser', function(){
+        return (Settings.find().count() == 0);
+    });
+
+    Template.newProfile.events = {
+        'submit': function(err, newProfile) {
+            var now = new Date();
+            var profile = {
+                name: newProfile.find("#name").value,
+                start: now,
+                sprint: now,
+                hours_per_day: parseInt(newProfile.find("#hpd").value),
+                days_per_week: parseInt(newProfile.find("#dpw").value)
+            };
+            Settings.insert(profile);
+        }
+    };
+
+    Template.leaderboard.sprints = function() {
+        return Sprint.findOne({},{ sort : { start : -1}});
+    }
+    // TODO: Rename this (but not sprint.goals, because sprint uses each goals, referring to different)
+    Template.sprint.goals = function () {
+        return Goals.find({_id: {$in : this.goals}});
+    };
+
+    // TODO: It's not liking the sorting the Date object
+    Template.sprint.sprint_empty = function () {
+        var curr_sprint = Sprints.findOne({},{ sort : { start : -1}});
+        if (curr_sprint === undefined)
+            return false;
+        return curr_sprint.goals.length === 0;
     };
 
     Template.leaderboard.selected_name = function () {
@@ -26,10 +65,22 @@ if (Meteor.isClient) {
     };
 
     Template.goal.tasks = function(){
-        //return Tasks.find({goal: this.name});
         var tasklist = Goals.findOne({name: this.name}).tasks;
         return Tasks.find({_id: { $in : tasklist} });
-        //return the lookup of all the tasks
+    }
+
+    Template.leaderboard.sprint = function() {
+        var now = new Date();
+        var ms_in_2_weeks = 24 * 7 * 2 * 60 * 60 * 1000;
+        var latest = Sprints.find({}, {sort: {start: -1}});
+        if ((!latest) ||now - latest.start > ms_in_2_weeks) {
+            latest = Sprint.insert({goals : [], start: nearestSunday()});
+            console.log(latest);
+            return Sprint.find({_id: latest});
+        } else {
+            return latest;
+        }
+        // TODO: Should we return the object instead of the cursor?
     }
 
     Template.goal.events({
@@ -45,6 +96,7 @@ if (Meteor.isClient) {
                 name: task.find("#name").value,
                 goal: task.find("#category").value,
                 hours: task.find("#hours").value,
+                hours_done: 0
             };
             var res = Tasks.insert(newTask);
             var goal_id = Goals.findOne({name: newTask.goal})._id
@@ -61,10 +113,9 @@ if (Meteor.isServer) {
         }
         var now = new Date();
 
-        var username = prompt('name'); //'test';
-        if (Settings.find({name: username}).count() === 0) {
+        /*if (Settings.find({name: username}).count() === 0) {
             Settings.insert({name: username, start: now, sprint: now, hours_per_day: 8, days_per_week: 5});
-        }
+        }*/
     });
 }
 Goals.find().fetch();
